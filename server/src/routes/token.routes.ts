e23fc6ce0d832f90a307ea1655b7cd7ff337e51a
Juno Yun <junoyun242@gmail.com>
2024-04-18 00:15:23 +0900

    Refactor code

--- server/src/routes/token.routes.ts
+++ server/src/routes/token.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { issueToken, validateToken } from "../controllers/token.controllers";
 const router = Router();
-``;
+
 router.get("/issue", issueToken);
 router.get("/validate/:token", validateToken);
 
