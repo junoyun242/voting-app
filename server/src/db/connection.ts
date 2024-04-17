d6da1e2250ad06056d8e9db465f67176febc0c82
Juno Yun <junoyun242@gmail.com>
2024-04-18 00:30:35 +0900

    Add env to db connection

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -2,10 +2,10 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  host: "127.0.0.1",
-  port: 5432,
-  user: "apeiron",
-  password: "1111",
-  database: "voting",
+  host: process.env.DATABASE_HOST || "",
+  port: Number(process.env.DATABASE_PORT) || 5432,
+  user: process.env.DATABASE_USER || "",
+  password: process.env.DATABASE_PASSWORD || "",
+  database: process.env.DATABASE_DATABASE || "",
 });
 export const db = drizzle(pool);
