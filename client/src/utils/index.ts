f86f234eefb29c0640ff78dd63182c33de115e1e
Juno Yun <junoyun242@gmail.com>
2024-04-22 01:44:31 +0900

    Update SERVER_URL to https

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = "http://dev.api.vote.junoyun242.org";
+export const SERVER_URL = "https://dev.api.vote.junoyun242.org";
 
 export const reqHeader = {
   "Content-Type": "application/json",
