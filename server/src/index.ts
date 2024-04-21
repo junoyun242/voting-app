e6241d77e469fed6d564aefba9a8d1c8bb57eccd
Juno Yun <junoyun242@gmail.com>
2024-04-21 22:31:01 +0900

    Update config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,14 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import TokenRoutes from "./routes/token.routes";
 import PollRoutes from "./routes/poll.routes";
 import VoteRoutes from "./routes/vote.routes";
 import expressWinston from "express-winston";
-import { join } from "path";
 import { loggerObj } from "./util/logger";
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
-app.use("/assets", express.static(join(process.cwd(), "client", "assets")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
@@ -24,9 +22,5 @@ app.use(
 app.use("/api/token", TokenRoutes);
 app.use("/api/poll", PollRoutes);
 app.use("/api/vote", VoteRoutes);
-
-app.get("*", (req: Request, res: Response) => {
-  res.sendFile(join(process.cwd(), "client", "index.html"));
-});
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
