ffabc583f3f6cb1412891a354cfff25878104702
Juno Yun <junoyun242@gmail.com>
2024-04-25 21:42:54 +0900

    Add warning for using pc

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,12 @@ import App from "./App.tsx";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
+import { Notifications } from "@mantine/notifications";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider defaultColorScheme="dark">
+      <Notifications />
       <ModalsProvider>
         <App />
       </ModalsProvider>
